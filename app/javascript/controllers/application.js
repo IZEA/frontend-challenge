28b1be069870b2b2b7b6cd7101c9b6f062805d31
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2023-08-22 14:28:17 -0400

    removed JS events for relaod, works with just turbo now

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,31 +5,6 @@ const application = Application.start()
 application.debug = false
 window.Stimulus = application
 
-document.addEventListener("turbo:load", function () {
-    document.addEventListener("turbo:stream", function (event) {
-        const detail = event.detail;
-        if (detail && detail.target && detail.template) {
-            const targetFrame = detail.target;
-            const template = detail.template;
-            const turboFrame = document.querySelector('[data-turbo-frame="image_upload"]');
-            if (turboFrame) {
-                turboFrame.innerHTML = template;
-            }
-        }
-    });
-    document.addEventListener("turbo:stream:remove", function (event) {
-        const detail = event.detail;
-        if (detail && detail.target) {
-            const targetFrame = detail.target;
-            const turboFrame = document.querySelector(`[data-turbo-frame="image_upload"]`);
-            if (turboFrame) {
-                turboFrame.remove();
-            }
-        }
-    });
-});
-
-
 export { application }
 
 
